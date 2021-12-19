30483bbc48c3446c2bd62162367f4b4543e5151e
isokolov <illya.sokolov82@gmail.com>
2021-12-19 21:40:28 +0200

    Login component, html template

--- backend-springboot/backend-springboot/frontend-angular/frontend-angular/src/app/app.component.spec.ts
+++ backend-springboot/backend-springboot/frontend-angular/frontend-angular/src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'frontend-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend-angular');
+    // expect(app.title).toEqual('frontend-angular');
   });
 
   it('should render title', () => {
--- backend-springboot/backend-springboot/frontend-angular/frontend-angular/src/app/app.component.ts
+++ backend-springboot/backend-springboot/frontend-angular/frontend-angular/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend-angular';
+export class AppComponent implements OnInit {
+
+  cookieEnabled: boolean; // будет хранить true или false - включены или отключены куки в браузере
+
+  // метод будем вызываться автоматически при иниц. компонента
+  ngOnInit(): void {
+
+    this.cookieEnabled = navigator.cookieEnabled; // проверяем включены ли куки в браузере
+
+    // попробовать установить тестовый кук - если не получится - значит куки не работают
+    if (!this.cookieEnabled) { // убеждаемся, что нельзя записать кук
+      document.cookie = 'testcookie';
+      this.cookieEnabled = (document.cookie.indexOf('testcookie') !== -1); // записываем в переменную true или false
+    }
+
+  }
 }
