2607ae47d33a3e5d5be990276c4c788dddbc7d73
isokolov <illya.sokolov82@gmail.com>
2021-12-19 22:28:02 +0200

    Take data from form

--- backend-springboot/backend-springboot/frontend-angular/frontend-angular/src/app/auth/login/login.component.ts
+++ backend-springboot/backend-springboot/frontend-angular/frontend-angular/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup; // форма с введенными пользователем данными
+
+
+  // внедрение всех нужных объектов
+  constructor(
+    private formBuilder: FormBuilder // для создание формы
+  ) {
+  }
 
   ngOnInit(): void {
+    // инициализация формы с нужными полями, начальными значениями и валидаторами
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required], // Validators.required - проверка на обязательность заполнения
+      password: ['', [Validators.required]],
+    });
+
+  }
+
+  // быстрый доступ на компоненты формы (для сокращения кода, чтобы каждый раз не писать this.form.get('') )
+  get usernameField(): AbstractControl {
+    return this.form.get('username');
+  }
+
+  get passwordField(): AbstractControl {
+    return this.form.get('password');
+  }
+
+
+  // попытка отправки данных формы аутентификации
+  submitForm(): void {
+
+    console.log(this.form); // пока просто считываем введенные данные
+    console.log(this.usernameField.value); // пока просто считываем введенные данные
+    console.log(this.passwordField.value); // пока просто считываем введенные данные
+
   }
 
 }
